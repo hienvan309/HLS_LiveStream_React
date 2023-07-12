7202692f3670a9232c7fe7b0e96b99e742ec6db7
Hien Van <namdinh3009@gmail.com>
2023-07-12 12:05:28 +0700

    update

---
+++ src/ReactVideoPlayer.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from 'react';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
+
+const ReactVideoPlayer = ({ videoSource }) => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const videoElement = videoRef.current;
+    const player = videojs(videoElement);
+
+    return () => {
+      player.dispose();
+    };
+  }, []);
+
+  return (
+    <div className="video-container">
+      <video ref={videoRef} className="video-js vjs-default-skin" controls>
+        <source src={videoSource} type="application/x-mpegURL" />
+      </video>
+    </div>
+  );
+};
+
+export default ReactVideoPlayer;
