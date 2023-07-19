102f02d3aa85f37431b8567d99cbdd88a4913599
Hien Van <namdinh3009@gmail.com>
2023-07-19 13:45:44 +0700

    Updated

--- src/ReactVideoPlayer.js
+++ src/ReactVideoPlayer.js
@@ -16,7 +16,7 @@ const ReactVideoPlayer = ({ videoSource }) => {
 
   return (
     <div className="video-container">
-      <video ref={videoRef} preload='datameta' className="video-js vjs-default-skin" controls>
+      <video ref={videoRef} preload='datameta' autoPlay className="video-js vjs-default-skin" controls>
         <source src={videoSource} type="application/x-mpegURL" />
       </video>
     </div>
