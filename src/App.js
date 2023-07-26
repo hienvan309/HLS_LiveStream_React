adcb816cf970f09e7f179c565ab032193d56c28e
Hien Van <namdinh3009@gmail.com>
2023-07-26 11:52:50 +0700

    Updated 26/7

--- src/App.js
+++ src/App.js
@@ -12,7 +12,18 @@ const App = () => {
       try {
         const response = await axios.get('/api/v1/event/get-cameras');
         const data = response.data;
+
+        // Chuyển đổi dữ liệu từ API vào định dạng phù hợp cho selectedCameras
+        const initialSelectedCameras = {};
+        data.forEach((camera) => {
+          initialSelectedCameras[camera.camera_id] = '';
+        });
+
         setCameraData(data);
+        setSelectedCameras(initialSelectedCameras);
+
+        // Log ra mảng cameraData để kiểm tra
+        console.log('cameraData:', data);
       } catch (error) {
         console.error(error);
       }
@@ -30,98 +41,31 @@ const App = () => {
   return (
     <div className="app">
       <div className="video-container">
-        <div className="video-player top-left">
-          <div className="dropdown-container">
-            <select
-              value={selectedCameras['top-left'] || ''}
-              onChange={(event) => handleCameraChange(event, 'top-left')}
-            >
-              <option value="" disabled>
-                Select a camera
-              </option>
-              {cameraData.map((camera, index) => (
-                <option key={index} value={camera.camera_hls_streaming_endpoint}>
-                  {camera.source_url}
+        {cameraData.map((camera) => (
+          <div className="video-player" key={camera.camera_id}>
+            <div className="dropdown-container">
+              <select
+                value={selectedCameras[camera.camera_id] || ''}
+                onChange={(event) => handleCameraChange(event, camera.camera_id)}
+              >
+                <option value="" disabled>
+                  Select a camera
                 </option>
-              ))}
-            </select>
+                {cameraData.map((cam) => (
+                  <option key={cam.camera_id} value={cam.camera_hls_streaming_endpoint}>
+                    {cam.source_url}
+                  </option>
+                ))}
+              </select>
+            </div>
+            {selectedCameras[camera.camera_id] && (
+              <ReactVideoPlayer
+                key={selectedCameras[camera.camera_id]}
+                videoSource={selectedCameras[camera.camera_id]}
+              />
+            )}
           </div>
-          {selectedCameras['top-left'] && (
-            <ReactVideoPlayer
-              key={selectedCameras['top-left']} // Thêm key vào ReactVideoPlayer
-              videoSource={selectedCameras['top-left']}
-            />
-          )}
-        </div>
-        <div className="video-player top-right">
-          <div className="dropdown-container">
-            <select
-              value={selectedCameras['top-right'] || ''}
-              onChange={(event) => handleCameraChange(event, 'top-right')}
-            >
-              <option value="" disabled>
-                Select a camera
-              </option>
-              {cameraData.map((camera, index) => (
-                <option key={index} value={camera.camera_hls_streaming_endpoint}>
-                  {camera.source_url}
-                </option>
-              ))}
-            </select>
-          </div>
-          {selectedCameras['top-right'] && (
-            <ReactVideoPlayer
-              key={selectedCameras['top-right']} // Thêm key vào ReactVideoPlayer
-              videoSource={selectedCameras['top-right']}
-            />
-          )}
-        </div>
-        <div className="video-player bottom-left">
-          <div className="dropdown-container">
-            <select
-              value={selectedCameras['bottom-left'] || ''}
-              onChange={(event) => handleCameraChange(event, 'bottom-left')}
-            >
-              <option value="" disabled>
-                Select a camera
-              </option>
-              {cameraData.map((camera, index) => (
-                <option key={index} value={camera.camera_hls_streaming_endpoint}>
-                  {camera.source_url}
-                </option>
-              ))}
-            </select>
-          </div>
-          {selectedCameras['bottom-left'] && (
-            <ReactVideoPlayer
-              key={selectedCameras['bottom-left']} // Thêm key vào ReactVideoPlayer
-              videoSource={selectedCameras['bottom-left']}
-            />
-          )}
-        </div>
-        <div className="video-player bottom-right">
-          <div className="dropdown-container">
-            <select
-              value={selectedCameras['bottom-right'] || ''}
-              onChange={(event) => handleCameraChange(event, 'bottom-right')}
-            >
-              <option value="" disabled>
-                Select a camera
-              </option>
-              {cameraData.map((camera, index) => (
-                <option key={index} value={camera.camera_hls_streaming_endpoint}>
-                  {camera.source_url}
-                </option>
-              ))}
-            </select>
-          </div>
-          {selectedCameras['bottom-right'] && (
-            <ReactVideoPlayer
-              key={selectedCameras['bottom-right']} // Thêm key vào ReactVideoPlayer
-              videoSource={selectedCameras['bottom-right']}
-            />
-          )}
-        </div>
+        ))}
       </div>
     </div>
   );
--- src/ReactVideoPlayer.js
+++ src/ReactVideoPlayer.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import videojs from 'video.js';
+import Hls from 'hls.js'; // Import thư viện hls.js
 import 'video.js/dist/video-js.css';
 
 const ReactVideoPlayer = ({ videoSource }) => {
@@ -7,17 +7,35 @@ const ReactVideoPlayer = ({ videoSource }) => {
 
   useEffect(() => {
     const videoElement = videoRef.current;
-    const player = videojs(videoElement);
+    let hls = null;
+
+    if (Hls.isSupported()) {
+      // Kiểm tra nếu trình duyệt hỗ trợ HLS
+      hls = new Hls();
+
+      // Tắt tính năng tự động tải các đoạn video sau khi play
+      // hls.config.autoStartLoad = false;
+
+      // Tắt tính năng tăng tốc phát lại
+      hls.config.capLevelToPlayerSize = false;
+
+      hls.loadSource(videoSource);
+      hls.attachMedia(videoElement);
+    } else if (videoElement.canPlayType('application/vnd.apple.mpegurl')) {
+      // Hỗ trợ trên Safari
+      videoElement.src = videoSource;
+    }
 
     return () => {
-      player.dispose();
+      if (hls) {
+        hls.destroy();
+      }
     };
-  }, []);
+  }, [videoSource]);
 
   return (
     <div className="video-container">
-      <video ref={videoRef} className="video-js vjs-default-skin" preload='metadata' controls>
-        <source src={videoSource} type="application/x-mpegURL" />
+      <video ref={videoRef} autoPlay muted className="video-js vjs-default-skin" preload='datameta' controls>
       </video>
     </div>
   );
